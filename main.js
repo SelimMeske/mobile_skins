57bedf414c104840df1fe94ea6a1c796dc08511f
SelimMeske <mesic.selim@outlook.com>
2021-05-22 22:51:55 +0200

    added material handler

--- main.js
+++ main.js
@@ -4,3 +4,12 @@ let carbon_black = "http://infinityskins.ba/wp-content/uploads/2021/05/carbon-bl
 let leather_black = "http://infinityskins.ba/wp-content/uploads/2021/05/leather-black.png";
 let wood_bamboo = "http://infinityskins.ba/wp-content/uploads/2021/05/wood-bamboo.png";
 let marble_white = "http://infinityskins.ba/wp-content/uploads/2021/05/marble-white.png";
+
+let all_materials = document.querySelectorAll('.material-card');
+
+for(let i=0; i<all_materials.length; i++) {
+    all_materials[i].addEventListener('click', e => {
+        let material = e.target.parentElement.id;
+        console.log(material);
+    });
+}
