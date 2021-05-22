fd11347b6d1402ee3edea271f515bfd9964d15cd
SelimMeske <mesic.selim@outlook.com>
2021-05-22 21:31:30 +0200

    added few links

---
+++ main.js
@@ -0,0 +1,6 @@
+//Skin links
+let carbon_red = "http://infinityskins.ba/wp-content/uploads/2021/05/carbon-red.png";
+let carbon_black = "http://infinityskins.ba/wp-content/uploads/2021/05/carbon-black.png";
+let leather_black = "http://infinityskins.ba/wp-content/uploads/2021/05/leather-black.png";
+let wood_bamboo = "http://infinityskins.ba/wp-content/uploads/2021/05/wood-bamboo.png";
+let marble_white = "http://infinityskins.ba/wp-content/uploads/2021/05/marble-white.png";
