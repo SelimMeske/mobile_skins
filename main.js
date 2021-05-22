8b3e08975977c183568fc6babe415813f2fe1a57
SelimMeske <mesic.selim@outlook.com>
2021-05-22 23:21:28 +0200

    more functionality

--- main.js
+++ main.js
@@ -6,10 +6,28 @@ let wood_bamboo = "http://infinityskins.ba/wp-content/uploads/2021/05/wood-bambo
 let marble_white = "http://infinityskins.ba/wp-content/uploads/2021/05/marble-white.png";
 
 let all_materials = document.querySelectorAll('.material-card');
+let back_skin_image = document.querySelector('.back-image');
 
 for(let i=0; i<all_materials.length; i++) {
     all_materials[i].addEventListener('click', e => {
         let material = e.target.parentElement.id;
-        console.log(material);
+        console.log(material)
+        switch(material){
+            case "carbon-red":
+                back_skin_image.href.baseVal = carbon_red;
+                break;
+            case "carbon-black":
+                back_skin_image.href.baseVal = carbon_black;
+                break;
+            case "leather-black":
+                back_skin_image.href.baseVal = leather_black;
+                break;
+            case "wood-bamboo":
+                back_skin_image.href.baseVal = wood_bamboo;
+                break;  
+            case "marble-white":
+                back_skin_image.href.baseVal = marble_white;
+                break;
+        }
     });
 }
