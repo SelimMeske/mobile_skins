e36682d90691148f921270cf4b746d701a28bcd8
SelimMeske <mesic.selim@outlook.com>
2021-06-20 17:27:15 +0200

    fixed few bugs and added a better logic

--- main.js
+++ main.js
@@ -11,7 +11,6 @@ let backButton = document.querySelector('.et-back-button');
 let mainPopupWindow = document.querySelector('.et-main-popup-window');
 let chooseCamSkinButton = document.querySelector('.et-card-camera-skin');
 let chooseBackSkinButton = document.querySelector('.et-card-back-skin');
-let ribbon = document.querySelectorAll('.et-add-ribbon');
 let addSkinButton = document.querySelector('.et-add-skin-button');
 let addSkinCards = document.querySelectorAll(".clicable-et-card");
 let backSkinCard = document.querySelector("#et-back-skin");
@@ -22,12 +21,17 @@ let camSkinCard = document.querySelector("#et-cam-skin");
 for(let p=0; p<addSkinCards.length; p++) {
     addSkinCards[p].addEventListener("click", e => {
 
+        //Remove all selected materials initially
+        remove_all_selected_material_cards(allMaterialCards);
+
         if (e.target.parentNode == backSkinCard) {
             mode = 'back_skin';
             mainPopupWindow.classList.remove('et-close-popup-window');
+            add_selected_material(allMaterialCards, choosedBackSkin);
         }else if (e.target.parentNode == camSkinCard) {
             mode = 'cam_skin';
             mainPopupWindow.classList.remove('et-close-popup-window');
+            add_selected_material(allMaterialCards, choosedCamSkin); 
         }
     });
 }
@@ -36,72 +40,78 @@ for(let i=0; i<allMaterials.length; i++) {
     allMaterials[i].addEventListener('click', e => {
         
         let parent_card = e.target.parentElement;
-        let material = parent_card.id;
-        
-        if (parent_card.classList.contains('et-card-selected')) {
+        let material = parent_card.id.replace('-', '_');
+
+
+        if (mode == 'back_skin' && material == choosedBackSkin) {
             remove_all_selected_material_cards(allMaterialCards);
-            if(mode == 'back_skin') {
-                choosedBackSkin = null;
-                backSkinImage.href.baseVal = "";
-            }else if(mode == 'cam_skin'){
-                choosedCamSkin = null;
-                camSkinImage.href.baseVal = "";
-            }
+            choosedBackSkin = null;
+            backSkinImage.href.baseVal = "";
+            
+        }else if(mode == 'cam_skin' && material == choosedCamSkin){
+            remove_all_selected_material_cards(allMaterialCards);
+            choosedCamSkin = null;
+            camSkinImage.href.baseVal = "";
         }else{
             remove_all_selected_material_cards(allMaterialCards);
-            parent_card.classList.add('et-card-selected');
+
             switch(material){
-                case "carbon-red":
+                case "carbon_red":
                     if (mode == 'back_skin'){
                         backSkinImage.href.baseVal = galaxyS21.carbonRedBack;
                         choosedBackSkin = 'carbon_red';
                     }else if(mode == 'cam_skin'){
                         camSkinImage.href.baseVal = galaxyS21.carbonRedCam;
+                        choosedCamSkin = 'carbon_red';
                     }
                     break;
-                case "carbon-black":
+                case "carbon_black":
                     if (mode == 'back_skin'){
                         backSkinImage.href.baseVal = galaxyS21.carbonBlackBack;
                         choosedBackSkin = 'carbon_black';
                     }else if(mode == 'cam_skin'){
                         camSkinImage.href.baseVal = galaxyS21.carbonBlackCam;
+                        choosedCamSkin = 'carbon_black';
                     }
                     break;
-                case "carbon-gray":
+                case "carbon_gray":
                     if (mode == 'back_skin'){
                         backSkinImage.href.baseVal = galaxyS21.carbonGrayBack;
                         choosedBackSkin = 'carbon_gray';
                     }else if(mode == 'cam_skin'){
                         camSkinImage.href.baseVal = galaxyS21.carbonGrayCam;
+                        choosedCamSkin = 'carbon_gray';
                     }
                     break;
-                case "carbon-yellow":
+                case "carbon_yellow":
                     if (mode == 'back_skin'){
                         backSkinImage.href.baseVal = galaxyS21.carbonYellowBack;
                         choosedBackSkin = 'carbon_yellow';
                     }else if(mode == 'cam_skin'){
                         camSkinImage.href.baseVal = galaxyS21.yellowCarbonCam;
+                        choosedCamSkin = 'carbon_yellow';
                     }
                     break;  
-                case "carbon-white":
+                case "carbon_white":
                     if (mode == 'back_skin'){
                         backSkinImage.href.baseVal = galaxyS21.carbonWhiteBack;
                         choosedBackSkin = 'carbon_white';
                     }else if(mode == 'cam_skin'){
                         camSkinImage.href.baseVal = galaxyS21.whiteCarbonCam;
+                        choosedCamSkin = 'carbon_white';
                     }
                     break;
             }
-            
+            if(mode == 'back_skin'){
+                add_selected_material(allMaterialCards, choosedBackSkin, parent_card);                    
+            }else if(mode == 'cam_skin'){
+                add_selected_material(allMaterialCards, choosedCamSkin, parent_card);                    
+            }
         }
 
         
     });
 }
-
-chooseBackSkinButton.addEventListener('click', e => {
-    mode = 'back_skin';
-});
 
 backButton.addEventListener('click', e => {
     apply_skin_and_close();
@@ -113,12 +123,26 @@ addSkinButton.addEventListener('click', e => {
 
 function apply_skin_and_close() {
 
-    if(choosedBackSkin) {
+    let addButtonClone = `
+    <span class="cart-button-image">
+            <img src="http://infinityskins.ba/wp-content/uploads/2021/06/shopping-cart-2.png" alt="">
+    </span>DODAJ`;
+
+    let chooseBackSkinButtonRibbon = backSkinCard.querySelector('.et-add-ribbon');
+    let chooseCamSkinButtonRibbon = camSkinCard.querySelector('.et-add-ribbon');
+
+    if(mode == 'back_skin' && choosedBackSkin){
         chooseBackSkinButton.classList.add('et-choosed');
-        ribbon.innerHTML = '<span>&#10003;</span>';
-    }else {
+        chooseBackSkinButtonRibbon.innerHTML = '<span>&#10003;</span>';
+    }else if(mode == 'cam_skin' && choosedCamSkin){
+        chooseCamSkinButton.classList.add('et-choosed');
+        chooseCamSkinButtonRibbon.innerHTML = '<span>&#10003;</span>';
+    }else if(mode == 'back_skin' && !choosedBackSkin){
+        chooseBackSkinButtonRibbon.innerHTML = addButtonClone;
         chooseBackSkinButton.classList.remove('et-choosed');
-        ribbon.innerHTML = 'DODAJ';
+    }else if(mode == 'cam_skin' && !choosedCamSkin){
+        chooseCamSkinButtonRibbon.innerHTML = addButtonClone;
+        chooseCamSkinButton.classList.remove('et-choosed');
     }
 
     mainPopupWindow.classList.add('et-close-popup-window');
@@ -127,5 +151,14 @@ function apply_skin_and_close() {
 function remove_all_selected_material_cards(elems){
     for(let l=0; l<elems.length; l++){
         elems[l].classList.remove('et-card-selected');
+    }
+}
+
+function add_selected_material(elems, choosedSkin){
+    for(let l=0; l<elems.length; l++){
+        choosedElem = elems[l];
+        if(choosedElem.id.replace('-', '_') == choosedSkin){
+            choosedElem.classList.add('et-card-selected');
+        }
     }
 }
